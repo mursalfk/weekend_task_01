ed1a52992675665e7d4b910266e30dfe949adf15
mursalfk <mursalfurqan@gmail.com>
2018-06-25 10:35:59 +0500

    Second Commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+
+function otherfield(){
+    var  x=document.getElementById("pop");
+    if (x.style.display==="none") {
+        x.style.display="block";       
+    } 
+    else {
+        x.style.display="none";
+    }
+}
+
+
+/*  Refferences
+For buttons : https://www.w3schools.com/tags/tag_button.asp
+For 
+*/
---
+++ readme.md
@@ -0,0 +1,8 @@
+This repository contains the weekend prohect given by sir sumair on 24/June/2018
+
+This is not complete
+It still lacks the basic requirement:
+The button moven down by the emergence of the popup.
+Most of the code is in CSS and HTML
+
+This prject is made by Mursal Furqan
