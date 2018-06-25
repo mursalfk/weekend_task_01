417212468a248131dc139e97473e713b896fdd98
mursalfk <mursalfurqan@gmail.com>
2018-06-25 12:14:07 +0500

    Button problem fixed

--- Script.js
+++ Script.js
@@ -1,16 +1,15 @@
-
-function otherfield(){
-    var  x=document.getElementById("pop");
-    if (x.style.display==="none") {
+//it maintains the popup appearence and disappearence
+function design(){
+    var  x=document.getElementById("text");   //declaring popping to a smaller variable
+    if (x.style.display==="none") {  //giving condition for showing the popup
         x.style.display="block";       
     } 
     else {
-        x.style.display="none";
+        x.style.display="none"; //hiding the popup
     }
 }
 
 
 /*  Refferences
-For buttons : https://www.w3schools.com/tags/tag_button.asp
-For 
+For buttons : https://www.w3schools.com/tags/tag_button.asp 
 */
